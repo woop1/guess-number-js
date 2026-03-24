e588d8487f7f33860b31b84d2206409dae0bd2fd
Angel <arengifo300@gmail.com>
2026-03-23 20:29:10 -0600

    feat: variables, tipos de datos e interacción con usuario

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+// --- Variables con let (pueden cambiar) ---
 console.log('¡Hola desde JavaScript!');
+
+let nombre = 'Angel';
+let edad = 21;
+let estaAprendiendo = 'Desarrollo Web';
+
+// --- Imprime cada variable ---
+console.log(nombre);
+console.log(edad);
+console.log(estaAprendiendo);
+
+// --- Verifica los tipos de datos ---
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof estaAprendiendo);
+
+// --- Variables con const (no pueden cambiar) ---
+const curso = 'code 101';
+const maxIntentos = 10;
+
+console.log(curso);
+console.log(maxIntentos);
+
+// --- Entrada del usuario ---
+ let nombreUsuario = prompt ('Cómo te llamas?');
+ let edadUsuario = prompt ('Cual es tu edad?');
+
+ console.log(nombreUsuario);
+ console.log(edadUsuario);
+
+alert('Hola ' + nombreUsuario + ', tienes ' + edadUsuario + ' años.')
+
+// --- Verifica en consola ---
+console.log('Nombre:', nombreUsuario);
+
+console.log(typeof nombreUsuario);
+console.log(typeof edadUsuario);
