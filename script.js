a9621c1eb1367dfcef117eda46117ca85e5968c9
Angel <arengifo300@gmail.com>
2026-03-23 20:33:09 -0600

    feat: operadores aritméticos, concatenación y template literals

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log('¡Hola desde JavaScript!');
 
 let nombre = 'Angel';
 let edad = 21;
-let estaAprendiendo = 'Desarrollo Web';
+let estaAprendiendo = true;
 
 // --- Imprime cada variable ---
 console.log(nombre);
@@ -36,3 +36,33 @@ console.log('Nombre:', nombreUsuario);
 
 console.log(typeof nombreUsuario);
 console.log(typeof edadUsuario);
+
+
+// --- Operadores aritméticos ---
+let a = 20;
+let b = 7;
+
+console.log('Suma:', a + b);
+console.log('Resta:', a - b);
+console.log('Multiplicación:', a * b);
+console.log('División:', a / b);
+console.log('Módulo:', a % b);
+
+// --- ¿Qué resultado esperas? ---
+console.log('5' + 3);
+console.log(5 + 3);
+console.log('5' - 3);
+
+// Forma 1: Concatenación con +
+let saludo1 = 'Hola ' + nombreUsuario + ', tienes ' + edadUsuario + ' años.';
+console.log(saludo1);
+
+let saludo2 = `Hola  ${nombreUsuario} tienes ${edadUsuario} años.`;
+console.log(saludo2);
+
+// --- Calculadora de edad ---
+let anioActual = 2026;
+let anioNacimiento = anioActual - Number(edadUsuario);
+
+let saludo3 = `Hola  ${nombreUsuario} naciste aproximadamente en el año ${anioNacimiento}.`;
+console.log(saludo3);
